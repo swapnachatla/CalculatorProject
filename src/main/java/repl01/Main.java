0473c17d64837c51b37679a9826d43eaf59c3f1d
Akbar300 <akbar@cybertekschool.com>
2020-03-23 16:03:57 -0400

    Main.java

---
+++ src/main/java/repl01/Main.java
@@ -0,0 +1,16 @@
+package repl01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.next() + " 300");
+
+
+    }
+
+}
